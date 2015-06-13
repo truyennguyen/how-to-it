b6bf18279b4e869adc397d42dd6c832a16890658
Nick Eagan <neagan26@gmail.com>
2015-06-13 12:51:16 -0700

    basic user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var eat = require('eat');
+
+var userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  basic: {
+    email: {type: String, required: true, unique: true},
+    password: {type: String}
+  }
+});
+
+userSchema.methods.generateHash = function(password, callback) {
+  bcrypt.genSalt(8, function(err, salt) {
+    bcrypt.hash(password, salt, null, function(err, hash) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: 'internal server error'});
+      }
+      callback(null, hash);
+    });
+  });
+};
+
+userSchema.methods.checkPassword = function(password, callback) {
+  bcrypt.compare(password, this.basic.password, function(err, res) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({msg: 'internal server error'});
+    }
+    callback(null, res);
+  });
+};
+
+userSchema.methods.generateToken = function(secret, callback) {
+  eat.encode({id: this._id}, secret, callback);
+};
+
+module.exports = mongoose.model('User', userSchema);
