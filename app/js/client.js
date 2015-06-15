3e73fe98bde30dbec080ac5b9c52556d6f1e17b4
Nick Eagan <neagan26@gmail.com>
2015-06-15 11:35:20 -0700

    add basic index and client outlines

---
+++ app/js/client.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('angular/angular');
+require('angular-route');
+require('angular-cookies');
+require('angular-base64');
+
+// module name is subject to change
+var tutorialApp = angular.module('tutorialApp', ['ngRoute', 'ngCookies', 'base64']);
+
+// controllers
+
+// services
+
+// directives
+
+tutorialApp.config(['$routeProvider', function($routeProvider) {
+  // routes go here
+}]);
